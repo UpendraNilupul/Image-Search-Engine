dea3944d67cce4c84fa1aa80138791d47bff0801
Upendra Nilupul <139964152+UpendraNilupul@users.noreply.github.com>
2025-06-20 16:29:22 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,41 @@
+const searchForm = document.getElementById("search-form");
+const searchBox = document.getElementById("search-box");
+const searchResult = document.getElementById("search-result");
+const showMoreBtn = document.getElementById("show-more-btn");
+
+const accessKey = "ibA_w1Z8G9jEoErkndCaZjSZW3mty98o4PNga-0HgJ0";
+
+let keyword = "";
+let page = 1;
+
+async function searchImages(){
+    keyword = searchBox.value;
+    const url = `https://api.unsplash.com/search/photos?page=${page}&query=${keyword}&client_id=${accessKey}&per_page=12`;
+
+    const response = await fetch(url);
+    const data = await response.json();
+
+    const results = data.results;
+
+    results.map((result)=>{
+        const image = document.createElement("img");
+        image.src = result.urls.small;
+
+        const imageLink = document.createElement("a");
+        imageLink.href = result.links.html;
+        imageLink.target = "_blank";
+
+        imageLink.appendChild(image);
+        searchResult.appendChild(imageLink);
+    });
+
+    showMoreBtn.style.display = "block";
+
+    //console.log(data);
+}
+
+searchForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+    page = 1;
+    searchImages();
+});
